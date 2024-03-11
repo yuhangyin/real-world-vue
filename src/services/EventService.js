55ffea8311c15e06b40e43d771d9aee65a3143a7
yuhangyin <71966612+yuhangyin@users.noreply.github.com>
2024-03-11 22:33:10 +0800

    Initial commit

---
+++ src/services/EventService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+const apiClient = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/yuhangyin/vue',
+    withCredentials: false,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default {
+    getEvents(){
+        return apiClient.get('/events')
+    },
+    getEvent(id){
+        return apiClient.get('/events/' + id)
+    }
+}
